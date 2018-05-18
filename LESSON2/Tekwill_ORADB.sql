1fceb2db31f3562d692987caad6f7a3cadad803e
rpapuc <rinat.papuc@endava.com>
2018-05-18 20:37:11 +0300

    added sql commands

---
+++ LESSON2/Tekwill_ORADB.sql
@@ -0,0 +1,42 @@
+
+--select * 
+--from employees
+
+--Select * 
+--from  EMPLOYEES
+
+/*Select distinct salary
+      ,DEPARTMENT_ID 
+      ,COMMISSION_PCT
+      ,EMAIL
+      ,'djnf'as new_collo
+From EMPLOYEES*/
+
+
+
+Select FIRST_NAME
+      ,LAST_NAME
+      ,salary * 1.1 
+      ,salary + (salary*10)/100
+From EMPLOYEES
+
+Select 10+15-5*2/7 
+from dual
+
+
+Select ((10+15)-5*2)/7 
+From dual
+
+/*  comentariu pentru mai multe rinduri */ 
+-- comentariu pentru un rind
+
+Select * 
+FROM  EMPLOYEES
+
+
+DESCRIBE EMPLOYEES
+
+
+
+
+
